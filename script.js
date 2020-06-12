65549a5a34b80caf580d6c2a104f839534df515e
MmarinoM <marino.michael.1990@gmail.com>
2020-06-12 09:48:08 +0200

    fix already answer

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 (function() {
 let number = Math.floor(Math.random() * 8) + 1;
+
+//variable Bonne réponse
 let newP = document.createElement("p");
-
-
-
+newP.className = "animate__animated animate__bounce good";
+newP.id = "casper";
+//variable reponse deja donnée 
+let newPalready = document.createElement("p");
+newPalready.className = "animate__animated animate__bounce already";
+newPalready.id = "casper2";
+//variable mauvaise réponse
+let newPwrong = document.createElement("p");
+newPwrong.className = "animate__animated animate__bounce wrong";
+newPwrong.id = "casper3";
 
 let input = document.getElementById("answer");
-newP.className = "animate__animated animate__bounce good";
+
  let ulList = document.getElementById("userGoodResponse");
 
 
@@ -47,33 +56,49 @@ let player = [
 
         player.forEach(element => {
 
-            if(number == element.nb && element.names.includes(guessedName)){
-                
-                //AFFICHER CHAQUE ELELEMNT DU TABLEAU TABLEANSWER EN LI DANS LE HTML
-                tableAnswer.push(number+" "+guessedName);
-                
-                var item = tableAnswer[tableAnswer.length-1];
-                var elem = document.createElement("li");
-                elem.innerHTML = item;
-                ulList.appendChild(elem);
-                     
-                
+            if(element.names.includes(guessedName) && number == element.nb  ){
+                if(tableAnswer.includes(number+" "+guessedName)){
+                    document.getElementById("userGoodResponse").insertAdjacentElement('beforebegin', newPalready).innerHTML = "réponse déjà donnée";
+                    setTimeout(function() {
+                          document.getElementById("casper2").innerHTML = "";
+                        },2000);
 
-                //ajoute une balise p avec la bonne reponse 
+                }else{
+                     //AFFICHER CHAQUE ELELEMNT DU TABLEAU TABLEANSWER EN LI DANS LE HTML
 
-                document.getElementById("userGoodResponse").insertAdjacentElement('beforebegin', newP).innerHTML = "bonne réponse";
-                
-                // document.getElementById("userGoodResponse").appendChild(newP).innerHTML = number +" "+guessedName;
-                
-                
+                    tableAnswer.push(number+" "+guessedName);
+                    var item = tableAnswer[tableAnswer.length-1];
+                    var elem = document.createElement("li");
+                    elem.innerHTML = item;
+                    ulList.appendChild(elem);
+                        
+                    
 
-                // changement de numéro si bonne réponse et refresh l'input
+                    //ajoute une balise p avec la bonne reponse 
 
-                number = Math.floor(Math.random() * 4) + 1;
-                document.getElementById("num").innerHTML = number;
+                    document.getElementById("userGoodResponse").insertAdjacentElement('beforebegin', newP).innerHTML = "bonne réponse";
+                    
+                    // faire disparaitre "bonne réponse" apres 2sec
+
+                    setTimeout(function() {
+                          document.getElementById("casper").innerHTML = "";
+                        },2000);
+
+                    // changement de numéro si bonne réponse et refresh l'input
+
+                    number = Math.floor(Math.random() * 8) + 1;
+                    document.getElementById("num").innerHTML = number;
+                    document.getElementById("answer").value = null;
+                    count++;
+                }
+               
+
+            }else{
+                document.getElementById("userGoodResponse").insertAdjacentElement('beforebegin', newPwrong).innerHTML = " Mauvaise réponse";
+                    setTimeout(function() {
+                          document.getElementById("casper3").innerHTML = "";
+                        },2000);
                 document.getElementById("answer").value = null;
-                count++;
-
             }
         });
         
